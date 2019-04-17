5237ec859627e36ddf826a7c62a836fb596bc87a
Satgawas007 <48801433+Satgawas007@users.noreply.github.com>
2019-04-17 11:01:50 +0530

    Updates

---
+++ precache-manifest.20e371d88d1b40009a8627b62abc3f7b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "162f3e57ff3b7d5d1c62",
+    "url": "/medicalassistant/static/css/main.036af769.chunk.css"
+  },
+  {
+    "revision": "accf8dff9ed73d7c79d7",
+    "url": "/medicalassistant/static/js/runtime~main.be0e538f.js"
+  },
+  {
+    "revision": "38d77552b0353684a208177482d5b6ee",
+    "url": "/medicalassistant/static/media/primeicons.38d77552.svg"
+  },
+  {
+    "revision": "df68b1fec14ecb2d0c95",
+    "url": "/medicalassistant/static/js/2.47c39d2b.chunk.js"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "162f3e57ff3b7d5d1c62",
+    "url": "/medicalassistant/static/js/main.7178500e.chunk.js"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/medicalassistant/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/medicalassistant/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/medicalassistant/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "b8eccb1059ea5faaf6d8b7d457ccfd09",
+    "url": "/medicalassistant/static/media/primeicons.b8eccb10.eot"
+  },
+  {
+    "revision": "473e2a746d3c151d7dcaa626a7c84c60",
+    "url": "/medicalassistant/static/media/primeicons.473e2a74.ttf"
+  },
+  {
+    "revision": "71bb3d79dcf18b45ae845409e7c2ada3",
+    "url": "/medicalassistant/static/media/primeicons.71bb3d79.woff"
+  },
+  {
+    "revision": "df68b1fec14ecb2d0c95",
+    "url": "/medicalassistant/static/css/2.2bd1e6aa.chunk.css"
+  },
+  {
+    "revision": "bc32a43f7bd490547415983c99e322b6",
+    "url": "/medicalassistant/index.html"
+  }
+];
